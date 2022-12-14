676405b834d6b8e6e84a3f2803e0f55f540108df
Ilia <polikarpovilja@mail.ru>
2022-12-14 22:23:05 +0300

    Firstcom

---
+++ 58/Program.cs
@@ -0,0 +1,80 @@
+﻿Console.Clear();
+
+int I = GetUserNumber("Введите число строк матрицы А: ");    
+int m = GetUserNumber("Введите число столбцов матрицы А: ");
+
+int mI = GetUserNumber("Введите число строк матрицы В: ");
+int n = GetUserNumber("Введите число столбцов матрицы В: ");
+
+string resultExist = string.Empty;
+if (m != mI)
+{                                                           
+    resultExist = "Умножение невозможно, матрицы не согласованы: число столбцов матрицы А не равно числу строк матрицы В";  
+
+}
+else
+{
+    resultExist = "Конец";
+    int[,] matrixA = GetMatrix(I, m, 0, 10);               
+    int[,] matrixB = GetMatrix(mI, n, 0, 10);               
+    int[,] matrixC = ABMultiplication(matrixA, matrixB);    
+    PrintArray(matrixA);
+    PrintArray(matrixB);
+    PrintArray(matrixC);
+
+}
+Console.WriteLine(resultExist);
+
+int GetUserNumber(string messageToUser)
+{
+    while (true)
+    {
+        Console.Write(messageToUser);
+        if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber > 0)
+            return userNumber;
+        Console.WriteLine("Ошибка! Введите целое число больше нуля");
+    }
+}
+
+int[,] GetMatrix(int m, int n, int minValue, int maxValue)
+{
+    int[,] resultMatrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultMatrix[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] ABMultiplication(int[,] matrA, int[,] matrB)
+{
+    int[,] matrixC = new int[matrA.GetLength(0), matrB.GetLength(1)];
+
+    for (int i = 0; i < matrixC.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixC.GetLength(1); j++)
+        {
+            for (int r = 0; r< matrA.GetLength(1); r++)
+            {
+                matrixC[i,j] += matrA[i,r]*matrB[r,j];
+            }
+        }
+    }
+    return matrixC;
+}
---
+++ 62/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Clear();
+
+int[,] array = GetArray(4, 4);
+PrintArray(array);
+Console.WriteLine();
+
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+
+    int temp = 1;
+    int rowStart = 0;
+    int columnStart = 0;
+    int rowEnd = arr.GetLength(0) - 1;
+    int columnEnd = arr.GetLength(1) - 1;
+    int i, j;
+
+    while (temp <= rows * columns)
+    {
+        i = rowStart;
+        for (j = columnStart; j < columnEnd + 1; j++)
+        {
+            arr[i, j] = temp++;
+        }
+        rowStart++;
+
+        j = columnEnd;
+        for (i = rowStart; i < rowEnd + 1; i++)
+        {
+            arr[i, j] = temp++;
+        }
+        columnEnd--;
+
+        i = rowEnd;
+        for (j = columnEnd; j >= columnStart; j--)
+        {
+            arr[i, j] = temp++;
+        }
+        rowEnd--;
+
+        j = columnStart;
+        for (i = rowEnd; i > columnStart; i--)
+        {
+            arr[i, j] = temp++;
+        }
+        columnStart++;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j].ToString("D2")} ");
+        }
+        Console.WriteLine();
+    }
+}
